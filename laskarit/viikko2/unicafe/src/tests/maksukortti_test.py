1e53a60ab73e48ffcf16bc5ab8266469b18305f6
kaarleol <kaarlenloota@gmail.com>
2023-11-14 17:43:55 +0200

    coverage in case its necessary

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,28 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_alussa_oikea_summa(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10)
+
+    def test_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(100)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 11)
+
+    def test_rahan_ottaminen_kun_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5)
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+
+    def test_otetaan_liikaa_rahaa(self):
+        self.maksukortti.ota_rahaa(1100)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10)
+        self.assertEqual(self.maksukortti.ota_rahaa(1100), False)
+
+    def test_maksukortti_palautus_string(self):
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
